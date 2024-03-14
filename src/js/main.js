95c2edcfa9c219a9845180200ff62ee730eaa443
Vetal <sonovetal@gmail.com>
2024-03-14 20:09:02 +0200

    added abount-us.scss file, added section styles

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,12 @@ import menuBurger from "./modules/burgerMenu.js";
 import setActive from "./modules/setActive.js";
 document.addEventListener('DOMContentLoaded', () => {
 	menuBurger();
-	setActive()
+	setActive({
+		parrentSelector: '.menu__body',
+		targetSelector: '.menu__link'
+	});
+	setActive({
+		parrentSelector: '.btns-block',
+		targetSelector: '.btns-block__button'
+	});
 });
