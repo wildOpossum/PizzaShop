06fc6fc8eb78deeb82b2a9159a6fcce6e645c332
Vetal <sonovetal@gmail.com>
2024-03-27 19:45:43 +0200

    modification of the tab function. Fixed some bugs

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,35 +1,25 @@
 'use strict';
 
 function tabs(){
-	const tabParrent = document.querySelector('.items-workshop__nav');
+	const tabParrent = document.querySelector('.items-pizza__navigation');
 
 	tabParrent.addEventListener('click', initTabs);
 
-	function initTabs(event){
-		const targetElem = event.target;
+	function initTabs (event){
+		const targetElem = event.target;		
 
-		if(targetElem.closest('.items-workshop__nav')){
-			const tabNavItem = targetElem.closest('.items-workshop__button');
-			if(!tabNavItem.classList.contains('active')){
-				const activeTabNavItem = document.querySelector('.items-workshop__button.active');
-				activeTabNavItem.classList.remove('active');
-				tabNavItem.classList.add('active');
-	
-				const tabItem = document.querySelectorAll('.items-workshop__body');
-				const tabItemActive = document.querySelector('.items-workshop__body.active');
-	
-				tabItemActive.classList.remove('active');
-				tabItem[getIndex(tabNavItem)].classList.add('active');
-				console.log(tabItem[getIndex(tabNavItem)]);			
-			}
+		if (targetElem.classList.contains('items-pizza__type') && !targetElem.classList.contains('active')) {
+			const activeElement = document.querySelector('.items-pizza__type.active');
+			const elements = document.querySelectorAll('.items-pizza__item');
+			const elementType = targetElem.dataset.pizzaType;						
+
+			activeElement.classList.remove('active');
+			targetElem.classList.add('active');
+
+			elements.forEach(element => {				
+				!elementType || element.dataset.pizzaType === elementType ? element.hidden = false : element.hidden = true;
+			});
 		}
-	
-	}
-
-	
-	function getIndex(element){	
-		console.log(Array.from(element.parentNode.children).indexOf(element));	
-		return Array.from(element.parentNode.children).indexOf(element);
 	}
 }
 export default tabs;
