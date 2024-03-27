2273009a9632be57061c4360882833789b9b20ba
Vetal <sonovetal@gmail.com>
2024-03-27 19:43:09 +0200

    modification of the function. Added try catch to catch possible errors

--- src/js/modules/setActive.js
+++ src/js/modules/setActive.js
@@ -1,16 +1,21 @@
 'use strict';
 
 function setActive ({parrentSelector, targetSelector}) {
-	const parrent = document.querySelector(parrentSelector);
+	try {
+		const parrent = document.querySelector(parrentSelector);
 
-	parrent.addEventListener('click', (event) => {
-		const navItem = event.target.closest(targetSelector);
-		if(!navItem.classList.contains('active')){
-			const activeNavItem = document.querySelector(`${targetSelector}.active`);
-			activeNavItem.classList.remove('active');
-			navItem.classList.add('active');
-		}
-	})
+		parrent.addEventListener('click', (event) => {
+			const navItem = event.target.closest(targetSelector);
+			if(!navItem.classList.contains('active')){
+				const activeNavItem = document.querySelector(`${targetSelector}.active`);
+				activeNavItem.classList.remove('active');
+				navItem.classList.add('active');
+			}
+		})
+	} catch (error) {
+		console.log(error, 'Selector not found in this PAGE');
+	}
+
 }
 
 export default setActive;
