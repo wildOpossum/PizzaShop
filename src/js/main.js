c8c975506ee92908e476a554a117ac55e1d40bfe
Vetal <sonovetal@gmail.com>
2024-04-04 13:57:20 +0300

    import modules to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,23 @@
 'use strict';
 import menuBurger from "./modules/burgerMenu.js"; 
 import setActive from "./modules/setActive.js";
+import tabs from "./modules/tabs.js";
+import setSelect from "./modules/setSelect.js";
+import setSearchForm from "./modules/searchForm.js";
+import initLoginForm from "./modules/loginForm.js";
+
 document.addEventListener('DOMContentLoaded', () => {
 	menuBurger();
 	setActive({
 		parrentSelector: '.menu__body',
-		targetSelector: '.menu__link'
+		targetSelector: '.menu__link',
 	});
 	setActive({
 		parrentSelector: '.btns-block',
 		targetSelector: '.btns-block__button'
 	});
+	tabs();
+	setSelect();
+	setSearchForm();
+	initLoginForm();	
 });
